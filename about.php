32a1f50596d14e3fa90e567b66d592906716eb7c
nnlxix <opalganteng777@gmail.com>
2022-04-26 00:26:26 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/uikit.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/uikit.js"></script>
+    <script src="https://unpkg.com/typewriter-effect@2.3.1/dist/core.js"></script>
+    <title>Unix TimeStamp</title>
+</head>
+<body>
+    <h1 class="uk-text-center uk-margin-xlarge-top uk-text-primary">What is Unix TimeStamp?</h1>
+    <br>
+    <p class="uk-text-center uk-margin-xlarge-left uk-margin-xlarge-right uk-text-primary">The unix time stamp is a way to track time as a running total of seconds. This count starts at the Unix Epoch on January 1st, 1970 at UTC. Therefore, the unix time stamp is merely the number of seconds between a particular date and the Unix Epoch. It should also be pointed out (thanks to the comments from visitors to this site) that this point in time technically does not change no matter where you are located on the globe. This is very useful to computer systems for tracking and sorting dated information in dynamic and distributed applications both online and client side.</p>
+    
+</body>
+</html>
